41fe38dd4dc813b8ea3293c1a35bd5222290f77a
mmengi18 <mmengi18@montgomerycollege.edu>
2022-07-28 10:35:48 -0400

    to be finihsed

---
+++ src/components/Ingredient.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Ingredient = (props) => {
+    // I want to render a background color
+    // I want to render the name of a ing
+    // if there is a key with this `name` extract it
+    const { name, color } = props.ingredient
+    return (
+        <p 
+            style={{backgroundColor: color}}
+            onClick={props.clickFunc}
+            id={props.itemKey}
+        >
+            {name}
+        </p>
+    )
+	
+}
+
+export default Ingredient
---
+++ src/components/IngredientList.js
@@ -0,0 +1,29 @@
+import React  from 'react'
+import Ingredient from './Ingredient'
+
+const IngredientList = (props) => {
+// map over the ing
+// mapped over and created from Ingredient Comp
+let allIngredients = props.ingredients.map((ing, idx) => (
+    <li key={idx}>
+        <Ingredient 
+            ingredient={ing}
+            clickFunc={props.add}
+            itemKey={idx}
+        />
+    </li>
+))
+return (
+    <>
+    <section>
+        <h3>Ingredients list</h3>
+        <ul>
+            {allIngredients}
+        </ul>
+    </section>
+    </>
+)
+
+}
+
+export default IngredientList
