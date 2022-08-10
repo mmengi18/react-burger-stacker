2133b0a588c046cb0431bc55fed7c606a8b9e4f8
mmengi18 <mmengi18@montgomerycollege.edu>
2022-08-10 17:06:28 -0400

    react classes

--- src/components/BurgerStack.js
+++ src/components/BurgerStack.js
@@ -1,29 +1,29 @@
-import React, { UserState} from "react";
+import React, { Component } from "react";
 import IngredientList from './IngredientList'
 import BurgerPane from './BurgerPane'
 
-const BurgerStack = () => {
-	// state = {
-	// }
-	const ingredients = [
-		{ name: 'Kaiser Bun', color: 'saddlebrown' },
-		{ name: 'Sesame Bun', color: 'sandybrown' },
-		{ name: 'Gluten Free Bun', color: 'peru' },
-		{ name: 'Lettuce Wrap', color: 'olivedrab' },
-		{ name: 'Beef Patty', color: '#3F250B' },
-		{ name: 'Soy Patty', color: '#3F250B' },
-		{ name: 'Black Bean Patty', color: '#3F250B' },
-		{ name: 'Chicken Patty', color: 'burlywood' },
-		{ name: 'Lettuce', color: 'lawngreen' },
-		{ name: 'Tomato', color: 'tomato' },
-		{ name: 'Bacon', color: 'maroon' },
-		{ name: 'Onion', color: 'lightyellow' },
-	]
-
+class BurgerStacker extends Component {
+	state = {
+		ingredients: [
+			{ name: 'Kaiser Bun', color: 'saddlebrown' },
+			{ name: 'Sesame Bun', color: 'sandybrown' },
+			{ name: 'Gluten Free Bun', color: 'peru' },
+			{ name: 'Lettuce Wrap', color: 'olivedrab' },
+			{ name: 'Beef Patty', color: '#3F250B' },
+			{ name: 'Soy Patty', color: '#3F250B' },
+			{ name: 'Black Bean Patty', color: '#3F250B' },
+			{ name: 'Chicken Patty', color: 'burlywood' },
+			{ name: 'Lettuce', color: 'lawngreen' },
+			{ name: 'Tomato', color: 'tomato' },
+			{ name: 'Bacon', color: 'maroon' },
+			{ name: 'Onion', color: 'lightyellow' },
+		],
+		burgerIngredients: [],
+	}
 
 	// add to the burger
 	// click on an ing and use the `event` to target it
-	const addToStack = (e) => {
+	addToStack = (e) => {
 		// grab the color
 		const ingColor = e.target.style.backgroundColor
 		// grab the name
@@ -39,7 +39,7 @@ const BurgerStack = () => {
 	}
 
 	// remove from burger
-    const removeFromStack = (e) => {
+    removeFromStack = (e) => {
         // select an ing by id
         const clickIndex = e.target.id
         // copy the whole burger
@@ -51,7 +51,7 @@ const BurgerStack = () => {
     }
 
 	// clear said burger
-    const clearBurger = () => {
+    clearBurger = () => {
         // set state back to an empty array
         // this.setState(() => {
         //     return {
@@ -75,4 +75,4 @@ const BurgerStack = () => {
 	}
 }
 
-export default BurgerStack
+export default BurgerStacker
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Ingredient = (props) => {
-    // I want to render a background color
-    // I want to render the name of a ing
-    // if there is a key with this `name` extract it
-    const { name, color } = props.ingredient
-    return (
-        <p 
-            style={{backgroundColor: color}}
-            onClick={props.clickFunc}
-            id={props.itemKey}
-        >
-            {name}
-        </p>
-    )
-	
+class Ingredient extends Component {
+	render() {
+		// I want to render a background color
+        // I want to render the name of a ing
+        // if there is a key with this `name` extract it
+        const { name, color } = this.props.ingredient
+		return (
+            <p 
+                style={{backgroundColor: color}}
+                onClick={this.props.clickFunc}
+                id={this.props.itemKey}
+            >
+                {name}
+            </p>
+        )
+	}
 }
 
 export default Ingredient
